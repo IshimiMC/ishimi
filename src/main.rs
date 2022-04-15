c51164e52ef3ad4b610c621d6d02f9a07f616020
Wuzado <23276409+Wuzado@users.noreply.github.com>
2022-04-15 16:54:01 +0200

    Revert "Make the conversion to string slice clearer"
    This reverts commit eca992325d7440691868ae9d24f3a477ed2e4f56.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let file_str = fs::read_to_string(&args.path).unwrap().as_str();
+    let file_str = &*fs::read_to_string(&args.path).unwrap();
 
     println!("{:}", file_str);
 
